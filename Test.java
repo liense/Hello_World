85e370e40499ba40226bbbc93a9c085eacea7bb2
liense <578045775@qq.com>
2017-08-25 00:01:06 +0800

    Add hello world script by java

---
+++ Test.java
@@ -0,0 +1,17 @@
+package com.zsoft.odr.test;
+
+import java.util.*;
+
+public class Test {
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello_World");
+
+	}
+
+}
